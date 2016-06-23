c114f3c1195b2548602259a8638732b9ba3e437e
nbkhope <ryumemaru@yahoo.com>
2016-06-23 10:05:25 -0700

    Add Home component

---
+++ Home.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+} from 'react-native';
+
+class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      accessToken: this.props.accessToken,
+    };
+  }
+
+  // redirect(routeName, token) {
+  //   this.props.navigator.push({
+  //     name: routeName,
+  //     passProps: {
+  //       accessToken: token,
+  //     },
+  //   });
+  // }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Welcome back to our System!
+        </Text>
+
+        <TouchableHighlight
+          style={styles.submitButton}
+          onPress={this.onLogoutPress.bind(this)}
+        >
+          <Text style={styles.buttonText}>
+            Logout
+          </Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  inputText: {
+    backgroundColor: "#8D79AE",
+    height: 50,
+    padding: 6,
+    borderWidth: 1,
+    borderColor: "#19053A",
+    marginBottom: 10,
+  },
+  submitButton: {
+    height: 50,
+    backgroundColor: "#482E74",
+    alignSelf: 'stretch',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: "#8D79AE",
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  error: {
+    color: "red",
+  },
+});
+
+export default Home;
